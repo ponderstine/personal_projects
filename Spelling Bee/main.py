e1ff00c3948fb0004593962e1d972908184bc25f
Ponder Stine <stine.ponder@gmail.com>
2022-05-08 13:05:48 -0600

    takes in word, and checks against set of valid words

--- Spelling Bee/main.py
+++ Spelling Bee/main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+from english_words import english_words_lower_set as words
 
 def main():
-    print("Hello, world!")
+    myWord = input("Type a valid word: ")
+    string = "You input: " + myWord
+    print(string)
+    if myWord in words:
+        print("You entered a valid word")
+    else:
+        print("You did NOT enter a valid word")
 
 if __name__ == "__main__":
     main()
