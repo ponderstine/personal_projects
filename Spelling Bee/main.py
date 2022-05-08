bbe9782131972558c0d2b97bf0e941f84662b104
Ponder Stine <stine.ponder@gmail.com>
2022-05-08 14:08:19 -0600

    Adding in functionality

--- Spelling Bee/main.py
+++ Spelling Bee/main.py
@@ -33,27 +33,51 @@ def main():
     while True:
         round += 1
         isBreak = False
+        bonus = True
         print(f"\nRound {round}: Your letters are: {mainLetter}\n{otherLetters}")
         myWord = input("Enter a word using your letters: ")
 
+        if len(myWord) < 4:
+            print("Words must be at least 4 letters long. Try again.")
+            continue
         # check if quitting the game
-        if myWord == "Quit":
+        elif myWord == "Quit":
             print(f"\nThanks for playing! Your final score is {score}.")
             break
-        elif mainLetter not in myWord:
+        
+        # lowercase all letters to compare to set of lower case english words. Must be done after checking for 'Quit' since capital 'Q' is used to distinguish from the word 'quit' in the game
+        myWord = myWord.lower()
+
+        # check if word contains main letter
+        if mainLetter not in myWord:
             print(f"{myWord} does not include the main letter.")
-            break
+            continue
         else:
+            # check that all letters in word are valid
             for c in myWord:
                 if c not in allLetters:
                     print(f"{myWord} contains unavailable letters")
                     isBreak = True
                     break
+
             if isBreak:
+                # word contained invalid letter so don't need to check against all words
                 continue
             elif myWord in words:
                 # valid word, increase score by length of word
                 score += len(myWord)
+        
+                # loop through all 7 available letters and if one is not present then no bonus, if get to end of 7 letters without missing a letter in the word then bonus stays True
+                for c in allLetters:
+                    if c not in myWord:
+                        bonus = False
+                        break
+                
+                # check for bonus if word contains all 7 letters at least once
+                if(bonus):
+                    score += 5
+                    print(f"{myWord} uses all the letters! Here's a bonus")
+                
                 myExc = choice(exclamations)
                 print(f"{myExc}, your score is now {score}")
             else:
